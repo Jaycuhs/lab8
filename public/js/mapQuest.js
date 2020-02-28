1f713f43492ea0b6a9b8c9936aa1883af1190125
Jaycuhs <notjacuscortes@gmail.com>
2020-02-27 18:53:53 -0800

    thirdparty API lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = '9YwDO916eeOxI56AzVslTbyVJk5Ig8sD';
+
+	var map = L.mapquest.map('map', {
+		center: [32.878781, -117.235947],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
